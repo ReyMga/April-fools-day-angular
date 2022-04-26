88b50ae12e9d761bd1e28c5e87f49e260819bcc3
Raquel Muriega <muriega.raquel@gmail.com>
2022-04-26 13:53:20 -0300

    fix

---
+++ src/app/Model/colors.model.ts
@@ -0,0 +1,11 @@
+export class ColorsModel {
+    purple: number = 0;
+    blue: number = 0;
+    green: number = 0;
+    yellow: number = 0;
+    orange: number = 0;
+    red: number = 0;
+    grey: number = 0;
+    white: number = 0;
+    myColor: string ='';
+  };
--- src/app/services/data/data-layer.service.spec.ts
+++ src/app/services/data/data-layer.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { MethodsService } from './methods.service';
+import { DataLayerService } from './data-layer.service';
 
 describe('MethodsService', () => {
-  let service: MethodsService;
+  let service: DataLayerService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(MethodsService);
+    service = TestBed.inject(DataLayerService);
   });
 
   it('should be created', () => {
